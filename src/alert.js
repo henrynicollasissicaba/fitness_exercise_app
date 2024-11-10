c4bcbb8ff35cc1c93b76af7af7a908d20910aca1
Henry Nicollas <henrynissicaba@gmail.com>
2024-11-10 12:53:53 -0300

    initial commit for update project to nextjs

--- src/alert.js
+++
@@ -1,9 +0,0 @@
-import Swal from 'sweetalert2';
-
-export const showErrorMessage = (message) => {
-    Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: `${message}`
-    });
-}
--- src/sanitize.js
+++
@@ -1,7 +0,0 @@
-import DOMPurify from 'dompurify';
-
-export const sanitize = (input) => {
-    const sanitizedInput = DOMPurify.sanitize(input)
-
-    return sanitizedInput
-}
